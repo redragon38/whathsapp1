fde09947ed1251418e325afc6574d40dad21d905
redragon38 <l.bonin2011@gmail.com>
2025-12-22 19:28:17 +0100

    aa

---
+++ sw.js
@@ -0,0 +1,17 @@
+// sw.js - Service Worker pour PWA
+const CACHE_NAME = 'fluxio-pro-v2';
+const urlsToCache = ['/', '/index.html'];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
